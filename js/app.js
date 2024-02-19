7961b327186e2f7386c58cc89854ec935d783711
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-02-19 18:13:13 +0300

    "image finding app"

---
+++ js/app.js
@@ -0,0 +1,51 @@
+const formWrapper = document.querySelector('.form-wrapper');
+const form = document.querySelector('#form');
+const searchInput = document.querySelector('#search-input');
+const buttonWrapper = document.querySelector('.button-wrapper');
+const searchButton = document.querySelector('#searchButton');
+const clearButton = document.querySelector('#clearButton');
+const imageListWrapper = document.querySelector('.image-list-wrapper');
+
+const runEventListener = () => {
+    form.addEventListener('submit', search);
+    clearButton.addEventListener('click', clear);
+};
+
+const search = (e) => {
+    const value = searchInput.value.trim();
+    fetch(`https://api.unsplash.com/search/photos?query=${value}`, {
+        method: 'GET',
+        headers: {
+            Authorization:
+                'Client-ID RrB76bERIISrElKBtSSYdd7LBgHQqjJVQrN83Dh8_T8',
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            Array.from(data.results).forEach((image) => {
+                addImageToUI(image.urls.small);
+            });
+        })
+        .catch((err) => console.log(err));
+    e.preventDefault();
+};
+
+const clear = () => {
+    searchInput.value = '';
+    imageListWrapper.innerHTML = '';
+};
+
+const addImageToUI = (url) => {
+    const div = document.createElement('div');
+    div.className = 'card';
+
+    const img = document.createElement('img');
+    img.setAttribute('src', url);
+    img.height = '400';
+    img.width = '400';
+
+    div.appendChild(img);
+    imageListWrapper.appendChild(div);
+};
+
+runEventListener();
